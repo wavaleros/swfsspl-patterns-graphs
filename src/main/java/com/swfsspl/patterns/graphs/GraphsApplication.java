97ead0e9b49b54f385c99c3dfd0c314c43ea82a5
wavaleros <wavaleros@gmail.com>
2020-10-04 22:01:01 -0500

    Initial commit

---
+++ src/main/java/com/swfsspl/patterns/graphs/GraphsApplication.java
@@ -0,0 +1,13 @@
+package com.swfsspl.patterns.graphs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swfsspl/patterns/graphs/GraphsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swfsspl.patterns.graphs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
